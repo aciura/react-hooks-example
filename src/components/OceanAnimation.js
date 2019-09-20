78fc58ac596f940b5485ea7e3d4748afeb9bb50a
Adrian Ciura <aciura@gmail.com>
2019-09-20 20:26:41 +0200

    move loadImages outside

--- src/components/OceanAnimation.js
+++ src/components/OceanAnimation.js
@@ -4,20 +4,25 @@ import React, { useMemo, useState, useEffect } from 'react'
 const ocean = require.context('../assets/ocean', /*useSubDirs*/ false)
 const NUM_OF_FILES = 21
 
+function loadImages(width, height) {
+  console.log('MEMO: loading images')
+
+  return Array(NUM_OF_FILES)
+    .fill(null)
+    .map((i, idx) => (idx + 1).toString().padStart(2, '0'))
+    .map(num => ocean(`./ocean${num}.png`))
+    .map((imgsrc, idx) => (
+      <img key={idx} src={imgsrc} alt="ocean" width={width} height={height} />
+    ))
+}
+
 export default function OceanAnimation({ width, height }) {
   const [currentImg, setCurrentImg] = useState(0)
 
-  const renderedImages = useMemo(() => {
-    console.log('MEMO: loading images')
-
-    return Array(NUM_OF_FILES)
-      .fill(0)
-      .map((i, idx) => (idx + 1).toString().padStart(2, '0'))
-      .map(num => ocean(`./ocean${num}.png`))
-      .map(imgsrc => (
-        <img src={imgsrc} alt="ocean" width={width} height={height} />
-      ))
-  }, [NUM_OF_FILES, ocean])
+  const renderedImages = useMemo(() => loadImages(width, height), [
+    width,
+    height,
+  ])
 
   useEffect(() => {
     const requestId = window.requestAnimationFrame(timestamp =>
